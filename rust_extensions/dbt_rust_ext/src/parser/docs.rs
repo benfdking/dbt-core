a29c3091a5b52a3cf3a9366513ba03fb8751c56e
Ben King <9087625+benfdking@users.noreply.github.com>
2025-08-21 18:24:58 +0200

    temp

--- rust_extensions/dbt_rust_ext/src/parser/docs.rs
+++ rust_extensions/dbt_rust_ext/src/parser/docs.rs
@@ -59,7 +59,7 @@ impl DocumentationParser {
         _ignored: Option<&str>,
     ) -> PyResult<String> {
         let project_name = self.project.project_name(py)?; // needs GIL + returns PyResult
-        Ok(format!("doc.{project_name}.{resource_name}"))
+        Ok(self.generate_unique_id_block(project_name, resource_name.to_string()))
     }
 
     fn parse_block(
@@ -67,28 +67,20 @@ impl DocumentationParser {
         py: Python<'_>,
         block: &Bound<'_, PythonFileBlock>,
     ) -> Vec<Documentation> {
-        let name = block_contents_get_name(block);
-        let unique_id = self.generate_unique_id(py, &name, None).unwrap();
-
+        let block: FileBlock = block.into();
+        let project_name: String = self.project.project_name(py).unwrap();
         let block_contents = block_contents_get_contents(block).unwrap();
-        let unstripped_conents = get_rendered(&block_contents, None);
-        let stripped_contents = unstripped_conents.trim();
+        let name = block_contents_get_name();
+        let unique_id = self.generate_unique_id(py, &name, None).unwrap();
+        let block_contents = block_contents_get_contents(block).unwrap();
 
-        let base_resource = BaseResource {
-            path: block_get_file_path_relative_path(block),
-            original_file_path: block_get_file_path_original_file_path(block),
-            name: name,
-            resource_type: NodeType::Documentation,
-            package_name: self.project.project_name(py).unwrap(),
-            unique_id: unique_id,
-        };
-        let documentation_resource = artifacts::resources::v1::documentation::Documentation::new(
-            base_resource.clone(),
-            stripped_contents.to_string(),
-        );
-        let doc = Documentation::new(base_resource, documentation_resource);
-
-        vec![doc]
+        return self.parse_block_internal(
+            project_name,
+             unique_id,
+             file_block_path_relative_path,
+             block_contents_get_name(block),
+             block_contents_get_contents(block).unwrap()
+            );
     }
 
     fn parse_file(&self, py: Python<'_>, file_block: &Bound<'_, PythonFileBlock>) -> PyResult<()> {
@@ -116,8 +108,9 @@ impl DocumentationParser {
             .map(|r| r.into())
             .collect::<Vec<FileBlock>>();
         println!("Blocks: {:?}", blocks);
+        let project_name = self.project.project_name(py).unwrap();
         for block in blocks {
-            let docs = self.parse_block_internal(&block.into());
+            let docs = self.parse_block_internal(project_name.clone(), &block.into());
             for doc in docs {
                 let result = manifest_add_doc(
                     py,
@@ -137,13 +130,45 @@ impl DocumentationParser {
 impl From<Box<dyn BlockSearchResult>> for FileBlock {
     fn from(block: Box<dyn BlockSearchResult>) -> Self {
         let block_contents = block.contents();
-        FileBlock { contents: block_contents }
+        FileBlock {
+            contents: block_contents,
+        }
     }
 }
 
 impl DocumentationParser {
-    fn parse_block_internal(&self, file_block: &FileBlock) -> Vec<Documentation> {
+    fn generate_unique_id_block(&self, project_name: String, resource_name: String) -> String {
+        format!("doc.{project_name}.{resource_name}")
+    }
 
-        unimplemented!("parse_block_internal not implemented")
+    fn parse_block_internal(
+        &self,
+        project_name: String,
+        file_block_original_file_path: String,
+        file_block_path_relative_path: String,
+        file_block_conents_name: String,
+        file_block_conents: String,
+    ) -> Vec<Documentation> {
+        let unique_id =
+            self.generate_unique_id_block(project_name.clone(), file_block_conents_name.clone());
+
+        let unstripped_conents = get_rendered(&file_block_conents, None);
+        let stripped_contents = unstripped_conents.trim();
+
+        let base_resource = BaseResource {
+            path: file_block_path_relative_path,
+            original_file_path: file_block_original_file_path,
+            name: file_block_conents_name,
+            resource_type: NodeType::Documentation,
+            package_name: project_name,
+            unique_id: unique_id,
+        };
+        let documentation_resource = artifacts::resources::v1::documentation::Documentation::new(
+            base_resource.clone(),
+            stripped_contents.to_string(),
+        );
+        let doc = Documentation::new(base_resource, documentation_resource);
+
+        vec![doc]
     }
 }
